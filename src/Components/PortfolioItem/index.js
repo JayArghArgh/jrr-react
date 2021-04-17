39a1405e1a4aa88c85632d845a126fd1607d8620
jayarghargh <jayarghargh@gmail.com>
2021-04-17 20:32:18 +1000

    display props for portfolio items

--- src/Components/PortfolioItem/index.js
+++ src/Components/PortfolioItem/index.js
@@ -3,19 +3,20 @@ const PortfolioItem = (props) => {
   return (
     <div className="col-sm-12 col-md-3">
       <div className="card">
-        <img id="image_1" src="img/emplectory.png" className="card-img-top" alt="Employee Directory" />
+        <img id="image_1" src={props.itemDetails.image} className="card-img-top" alt={props.itemDetails.image_alt} />
           <div className="card-body">
-            <p className="card-text">Emplectory..</p>
+            <p className="card-text">{props.itemDetails.name}</p>
             <ul>
+              {/*Add list component*/}
               <li>React</li>
               <li>Axios</li>
               <li>API</li>
             </ul>
             <ul className="fa-ul">
               <li><span className="fa-li"><i className="fab fa-internet-explorer custom-li"></i></span><a
-                href="https://fsc-portfolio.github.io/emplectory/" target="_blank">View</a></li>
+                href={props.itemDetails.deploy.deploy_url} target="_blank">Deployed on {props.itemDetails.deploy.deploy_name}</a></li>
               <li><span className="fa-li"><i className="fab fa-github custom-li"></i></span><a
-                href="https://github.com/FSC-Portfolio/emplectory" target="_blank">GitHub</a></li>
+                href={props.itemDetails.repo.repo_url} target="_blank">{props.itemDetails.repo.repo_name}</a></li>
             </ul>
           </div>
       </div>
